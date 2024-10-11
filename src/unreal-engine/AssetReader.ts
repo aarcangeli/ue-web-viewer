dcd181d97c7c920c0ff10f5f2e343b41ff2c780d
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2024-10-11 22:52:04 +0200

    remove unused method

--- src/unreal-engine/AssetReader.ts
+++ src/unreal-engine/AssetReader.ts
@@ -199,13 +199,6 @@ export class AssetReader {
     return this._littleEndian;
   }
 
-  /**
-   * Returns the offset of the reader in the global buffer.
-   */
-  private get globalOffset() {
-    return this.dataView.byteOffset + this.offset;
-  }
-
   private makeChild(newBlob: DataView) {
     const reader = new AssetReader(newBlob);
     reader._littleEndian = this._littleEndian;
