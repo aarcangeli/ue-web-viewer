83de1f28220116933778a1de3b1d5752ad8758b9
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2025-04-04 19:13:49 +0200

    Things

--- src/unreal-engine/properties/NativeStructs.ts
+++ src/unreal-engine/properties/NativeStructs.ts
@@ -1,15 +1,15 @@
 import type { FBox } from "../modules/CoreUObject/structs/Box";
-import { FDateTime } from "../modules/CoreUObject/structs/DateTime";
-import { FFrameNumber } from "../modules/CoreUObject/structs/FrameNumber";
+import type { FDateTime } from "../modules/CoreUObject/structs/DateTime";
+import type { FFrameNumber } from "../modules/CoreUObject/structs/FrameNumber";
 import type { FGuid } from "../modules/CoreUObject/structs/Guid";
 import type { FLinearColor } from "../modules/CoreUObject/structs/LinearColor";
 import type { FMatrix44 } from "../modules/CoreUObject/structs/Matrix44";
 import type { FRotator } from "../modules/CoreUObject/structs/Rotator";
 import type { FSoftObjectPath } from "../modules/CoreUObject/structs/SoftObjectPath";
-import { FTransform } from "../modules/CoreUObject/structs/Transform";
-import { FTwoVectors } from "../modules/CoreUObject/structs/TwoVectors";
+import type { FTransform } from "../modules/CoreUObject/structs/Transform";
+import type { FTwoVectors } from "../modules/CoreUObject/structs/TwoVectors";
 import type { FVector2 } from "../modules/CoreUObject/structs/Vector2";
-import { FVector3 } from "../modules/CoreUObject/structs/Vector3";
+import type { FVector3 } from "../modules/CoreUObject/structs/Vector3";
 import type { FVector4 } from "../modules/CoreUObject/structs/Vector4";
 
 /**
