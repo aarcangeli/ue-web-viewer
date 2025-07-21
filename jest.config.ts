8e5030ac6c487d3239c8d13a399e7a34f0f0271c
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2025-07-21 02:24:21 +0200

    collect coverage from all files

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,8 @@ const isDebugAttached = Boolean(inspector.url());
 const config: Config = {
   ...defaults,
   preset: "ts-jest/presets/js-with-ts",
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  coveragePathIgnorePatterns: ["/__tests__/"],
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
