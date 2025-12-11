2eb7bc7cdc4b545875e9dea2ebb8806979245d66
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2025-12-11 21:50:19 +0100

    boh

--- src/UeWebViewer/viewer/preview/AssetPreview.tsx
+++ src/UeWebViewer/viewer/preview/AssetPreview.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 import React, { useEffect, useRef } from "react";
 import { UStaticMesh } from "../../../unreal-engine/modules/Engine/objects/StaticMesh";
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
 
 export function AssetPreview(props: { asset: Asset }) {
   const exportedObjects = props.asset.mainObject;
@@ -31,21 +31,29 @@ function MeshViewer(props: { mesh: UStaticMesh }) {
     }
 
     // Create the scene, camera, and renderer
-    const width = container.clientWidth;
-    const height = container.clientHeight;
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(90, width / height, 0.01);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
+    const env = makeEnv();
+
+    const scene = env.scene;
+    const renderer = env.renderer;
+
+    const camera = new THREE.PerspectiveCamera(90, 1, 0.01);
+
+    const resizeHandler = () => {
+      const newWidth = container.clientWidth;
+      const newHeight = container.clientHeight;
+      renderer.setSize(newWidth, newHeight, false);
+      camera.aspect = newWidth / newHeight;
+      camera.updateProjectionMatrix();
+    };
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    const material = new THREE.MeshMatcapMaterial({ color: 0xffffff });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
+    const controls = new PointerLockControls(camera, renderer.domElement);
     // Approx 1.1 per scroll step
-    controls.zoomSpeed = 2;
+    //controls.zoomSpeed = 2;
 
     camera.position.z = 5;
 
@@ -54,25 +62,18 @@ function MeshViewer(props: { mesh: UStaticMesh }) {
       renderer.render(scene, camera);
     });
 
-    const resizeHandler = () => {
-      const newWidth = container.clientWidth;
-      const newHeight = container.clientHeight;
-      renderer.setSize(newWidth, newHeight, false);
-      camera.aspect = newWidth / newHeight;
-      camera.updateProjectionMatrix();
-      console.log("Resized to", newWidth, newHeight);
-    };
-
     renderer.domElement.style.width = "100%";
     renderer.domElement.style.height = "100%";
     renderer.domElement.style.display = "block";
 
     container.appendChild(renderer.domElement);
     window.addEventListener("resize", resizeHandler);
+    resizeHandler();
+
     return () => {
       container.removeChild(renderer.domElement);
       window.removeEventListener("resize", resizeHandler);
-      renderer.dispose();
+      returnEnv(env);
     };
   });
 
@@ -101,4 +102,31 @@ function Placeholder(pros: { children: string }) {
       </Text>
     </Box>
   );
+}
+
+interface ThreeEnv {
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+}
+
+const pool: Array<ThreeEnv> = [];
+
+function makeEnv(): ThreeEnv {
+  if (pool.length > 0) {
+    console.log("Reusing scene from pool");
+    return pool.pop()!;
+  }
+
+  console.log("Creating new scene");
+  const scene = new THREE.Scene();
+  const renderer = new THREE.WebGLRenderer();
+  return { scene, renderer };
+}
+
+function returnEnv(scene: ThreeEnv) {
+  console.log("Returning scene to pool");
+  pool.push(scene);
+  scene.renderer.dispose();
+  scene.scene.clear();
+  scene.renderer.setAnimationLoop(null);
 }
