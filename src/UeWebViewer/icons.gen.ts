458b52595f1462bcefb625d6afd58656729faaa8
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2025-12-26 22:47:48 +0100

    add more icons

--- src/UeWebViewer/icons.gen.ts
+++ src/UeWebViewer/icons.gen.ts
@@ -1,2 +1,6 @@
+export { default as BlueprintIcon } from "./icons/Blueprint.svg?react";
 export { default as ConfigIcon } from "./icons/Config.svg?react";
+export { default as ContentDirIcon } from "./icons/ContentDir.svg?react";
 export { default as DirectoryIcon } from "./icons/Directory.svg?react";
+export { default as GenericFileIcon } from "./icons/GenericFile.svg?react";
+export { default as UnknownFileIcon } from "./icons/UnknownFile.svg?react";
