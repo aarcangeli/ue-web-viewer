bb58e68c62837257c6473edd5a292cb4d6aee458
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2025-12-29 17:00:42 +0100

    add FAnimPhysObjectVersion

--- src/unreal-engine/versioning/ue-custom-versions.ts
+++ src/unreal-engine/versioning/ue-custom-versions.ts
@@ -3,6 +3,7 @@
 // noinspection JSUnusedGlobalSymbols
 //
 
+import { FAnimPhysObjectVersionGuid } from "./custom-versions-enums/FAnimPhysObjectVersion";
 import { FEditorObjectVersionGuid } from "./custom-versions-enums/FEditorObjectVersion";
 import { FEnterpriseObjectVersionGuid } from "./custom-versions-enums/FEnterpriseObjectVersion";
 import { FFortniteMainBranchObjectVersionGuid } from "./custom-versions-enums/FFortniteMainBranchObjectVersion";
@@ -10,6 +11,7 @@ import { FUE5MainStreamObjectVersionGuid } from "./custom-versions-enums/FUE5Mai
 import type { CustomVersionGuid } from "./CustomVersionGuid";
 
 export const allCustomVersions: ReadonlyArray<CustomVersionGuid> = [
+  FAnimPhysObjectVersionGuid,
   FEditorObjectVersionGuid,
   FEnterpriseObjectVersionGuid,
   FFortniteMainBranchObjectVersionGuid,
